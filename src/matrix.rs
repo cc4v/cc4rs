1dfe8181eb8d54318e03b53b11e8ffe8bf566d78
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2026-09-18 07:15:06 +0900

    update

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 use sokol::gl as sgl;
 
 pub fn scale(x: f32, y: f32, z: f32) {
